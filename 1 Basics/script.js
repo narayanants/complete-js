e4935abc8804f88cb9acd25ddc81736ad191e3f6
Narayanan Ts <tsnarayanan7@gmail.com>
2017-12-27 21:39:59 +0530

    JS intro

---
+++ 1 Basics/script.js
@@ -0,0 +1,16 @@
+/*
+	var name = 'john';
+	console.log(name);
+
+	var lastName = "Smith";
+	console.log(lastName);
+
+	var age = 26;
+	console.log(age);
+
+	var fullAge = true;
+	console.log(fullAge);
+*/
+
+/* Lecture Variable 2 */
+
